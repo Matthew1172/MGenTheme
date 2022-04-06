2e06cb0bbca45212d0388e4250076e3d76c4de7a
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-06 02:17:50 -0400

    fixing functions.php

--- functions.php
+++ functions.php
@@ -7,6 +7,56 @@
  * @since 1.0.0
  */
 
+/**
+ * First, let's set the maximum content width based on the theme's design and stylesheet.
+ * This will limit the width of all uploaded images and embeds.
+ */
+if ( ! isset( $content_width ) )
+    $content_width = 800; /* pixels */
+
+if ( ! function_exists( 'mgen_setup' ) ) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which runs
+     * before the init hook. The init hook is too late for some features, such as indicating
+     * support post thumbnails.
+     */
+    function mgen_setup() {
+
+        /**
+         * Make theme available for translation.
+         * Translations can be placed in the /languages/ directory.
+         */
+        load_theme_textdomain( 'mgen', get_template_directory() . '/languages' );
+
+        /**
+         * Add default posts and comments RSS feed links to &lt;head>.
+         */
+        add_theme_support( 'automatic-feed-links' );
+
+        /**
+         * Enable support for post thumbnails and featured images.
+         */
+        add_theme_support( 'post-thumbnails' );
+
+        /**
+         * Add support for two custom navigation menus.
+         */
+        register_nav_menus( array(
+            'primary'   => __( 'Primary Menu', 'mgen' ),
+            'secondary' => __('Secondary Menu', 'mgen' )
+        ) );
+
+        /**
+         * Enable support for the following post formats:
+         * aside, gallery, quote, image, and video
+         */
+        add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+    }
+endif; // mgen_setup
+add_action( 'after_setup_theme', 'mgen_setup' );
+
 /*
  *
  * Hook into css and js
@@ -15,7 +65,7 @@
 function index_style_enqueue()
 {
     $theme_version = wp_get_theme()->get( 'Version' );
-    wp_enqueue_style( 'mgen-style', get_stylesheet_uri(), array(), $theme_version );
+    wp_enqueue_style( 'mgenStyle', get_stylesheet_uri(), array(), $theme_version );
 
 }
 add_action('wp_enqueue_scripts', 'index_style_enqueue');
