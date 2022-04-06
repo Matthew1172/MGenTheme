2254486b1cd3161df01d346be7a10c99b28d9338
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-06 15:29:22 -0400

    fixing bugs in js response

--- header.php
+++ header.php
@@ -10,6 +10,18 @@
  * @since 1.0.0
  */
 
+
+
+$api = '134.74.112.18';
+$endpoint = "/midi";
+$port = '1234';
+$folder = "output@WedApr61452382022";
+$file = "generated_1.mid";
+$url = "http://$api:$port$endpoint?folder=$folder&file=$file";
+
+$xml =  file_get_contents($url);
+echo $xml;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
