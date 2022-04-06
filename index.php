b31652f7d79b4ca8322c0f65ac6fe74299ef67c6
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-06 02:24:39 -0400

    changed index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-get_header();
-get_footer();
+<?php get_header(); ?>
+    <main>
+        <div class="controls">
+            <button id="btn-play">Play</button>
+            <button id="btn-pause">Pause</button>
+            <button id="btn-stop">Stop</button>
+        </div>
+        <div>
+            <h2 id="loading">Loading</h2>
+        </div>
+        <div id="score"></div>
+    </main>
+<?php get_footer(); ?>
