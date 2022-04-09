7127e443353361c8107a140e10c7fa5c17a341cd
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-09 17:36:51 -0400

    decode json from server in php

--- functions.php
+++ functions.php
@@ -175,7 +175,8 @@ function run_model()
     );
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
+    $result = utf8_encode($result);
     if ($result === FALSE) { /* Handle error */ }
-    wp_send_json($result);
+    wp_send_json(json_decode($result));
 }
 add_action('wp_ajax_call_run_model', 'run_model');
