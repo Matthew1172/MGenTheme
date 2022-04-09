8cf904a4854f05cada8141fd1cb234b7c33d9b1f
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-09 16:46:42 -0400

    replaced curl in php with stream context create

--- functions.php
+++ functions.php
@@ -165,10 +165,17 @@ function run_model()
         'temperature' => esc_attr($_POST['temperature']),
     );
 
-
-
-    $result['r'] = "hello";
-
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($callee_data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
     wp_send_json($result);
 }
 add_action('wp_ajax_call_run_model', 'run_model');
