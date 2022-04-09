a848708899459a63d71b3229fa768e8494e5b3a9
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-09 17:10:50 -0400

    added newline to php api call

--- functions.php
+++ functions.php
@@ -168,7 +168,7 @@ function run_model()
     // use key 'http' even if you send the request to https://...
     $options = array(
         'http' => array(
-            'header'  => "Content-type: application/json",
+            'header'  => "Content-type: application/json\r\n",
             'method'  => 'POST',
             'content' => http_build_query($callee_data)
         )
