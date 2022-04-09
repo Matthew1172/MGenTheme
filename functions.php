0ff0a6d7ba11575cd8a79c3f9ec37e423a5dbfd8
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-09 17:50:11 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -176,7 +176,8 @@ function run_model()
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
     if ($result === FALSE) { /* Handle error */ }
-    $result = json_encode($result);
+    $result = utf8_decode($result);
+    //$result = json_encode($result);
     wp_send_json($result);
 }
 add_action('wp_ajax_call_run_model', 'run_model');
