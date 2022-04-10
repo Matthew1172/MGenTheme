2fee3ca886ff14e8caa450f02360277e9435c378
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-10 03:03:23 -0400

    moved code to predictSong.js

--- inc/js/app.js
+++ inc/js/app.js
@@ -23,22 +23,4 @@ registerButtonEvents(audioPlayer);
 })();
  */
 
-function registerButtonEvents(audioPlayer) {
-    document.getElementById("btn-play").addEventListener("click", () => {
-        if (audioPlayer.state === "STOPPED" || audioPlayer.state === "PAUSED") {
-            audioPlayer.play();
-        }
-    });
-    document.getElementById("btn-pause").addEventListener("click", () => {
-        if (audioPlayer.state === "PLAYING") {
-            audioPlayer.pause();
-        }
-    });
-    document.getElementById("btn-stop").addEventListener("click", () => {
-        if (audioPlayer.state === "PLAYING" || audioPlayer.state === "PAUSED") {
-            audioPlayer.stop();
-            //osmd.cursor.reset();
-        }
-    });
-}
 
--- inc/js/predictSong.js
+++ inc/js/predictSong.js
@@ -160,6 +160,26 @@ function play_and_render(mxl){
 
 }
 
+function registerButtonEvents(audioPlayer) {
+    document.getElementById("btn-play").addEventListener("click", () => {
+        if (audioPlayer.state === "STOPPED" || audioPlayer.state === "PAUSED") {
+            audioPlayer.play();
+        }
+    });
+    document.getElementById("btn-pause").addEventListener("click", () => {
+        if (audioPlayer.state === "PLAYING") {
+            audioPlayer.pause();
+        }
+    });
+    document.getElementById("btn-stop").addEventListener("click", () => {
+        if (audioPlayer.state === "PLAYING" || audioPlayer.state === "PAUSED") {
+            audioPlayer.stop();
+            //osmd.cursor.reset();
+        }
+    });
+}
+
+
 /*
                     if(first && notes.length < 1){
 
