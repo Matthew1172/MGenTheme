026ec2a27b9de2162763f4cf979e1c8f6811cf3b
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-09 23:12:18 -0400

    Update predictSong.js

--- inc/js/predictSong.js
+++ inc/js/predictSong.js
@@ -100,33 +100,32 @@
                 success: function (response) {
                     switch (response['r']) {
                         case "Good":
-                            (async (mxl) => {
-                                var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdCanvas", {
-                                    // set options here
-                                    backend: "svg",
-                                    drawFromMeasureNumber: 0,
-                                    drawUpToMeasureNumber: Number.MAX_SAFE_INTEGER // draw all measures, up to the end of the sample
-                                });
-                                const audioPlayer = new OsmdAudioPlayer();
-                                osmd
-                                    .load(mxl)
-                                    .then(
-                                        function() {
-                                            window.osmd = osmd; // give access to osmd object in Browser console, e.g. for osmd.setOptions()
-                                            //console.log("e.target.result: " + e.target.result);
-                                            osmd.render();
-                                            // osmd.cursor.show(); // this would show the cursor on the first note
-                                            // osmd.cursor.next(); // advance the cursor one note
-                                        }
-                                    );
-                                await audioPlayer.loadScore(mxl);
-                                audioPlayer.on("iteration", notes => {
-                                    console.log(notes);
-                                });
+                            mxl = atob(response['scoreXml'])
+                            var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdCanvas", {
+                                // set options here
+                                backend: "svg",
+                                drawFromMeasureNumber: 0,
+                                drawUpToMeasureNumber: Number.MAX_SAFE_INTEGER // draw all measures, up to the end of the sample
+                            });
+                            const audioPlayer = new OsmdAudioPlayer();
+                            osmd
+                                .load(mxl)
+                                .then(
+                                    function() {
+                                        window.osmd = osmd; // give access to osmd object in Browser console, e.g. for osmd.setOptions()
+                                        //console.log("e.target.result: " + e.target.result);
+                                        osmd.render();
+                                        // osmd.cursor.show(); // this would show the cursor on the first note
+                                        // osmd.cursor.next(); // advance the cursor one note
+                                    }
+                                );
+                            audioPlayer.loadScore(mxl);
+                            audioPlayer.on("iteration", notes => {
+                                console.log(notes);
+                            });
 
-                                hideLoadingMessage();
-                                registerButtonEvents(audioPlayer);
-                            })(atob(response['scoreXml']));
+                            hideLoadingMessage();
+                            registerButtonEvents(audioPlayer);
                             break;
                         case "Bad":
                             alert("Bad");
